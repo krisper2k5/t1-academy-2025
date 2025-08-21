07eb7602594a5936ddf4e1aa897f791fa5377bba
Кормилицын Илья Сергеевич <kormilitsyn.ilya@mail.ru>
2025-08-21 20:00:19 +0700

    Add localStorage from toggle theme

--- src/hooks/use-theme.tsx
+++ src/hooks/use-theme.tsx
@@ -2,11 +2,15 @@ import { useLayoutEffect, useState } from "react";
 
 type Theme = "dark" | "light";
 
-export const useTheme = (): [string, (theme: Theme) => void] => {
-  const [theme, setTheme] = useState<Theme>("light");
+export const useTheme = (): [Theme, (theme: Theme) => void] => {
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedData = localStorage.getItem("app-theme") as Theme;
+    return savedData || "light";
+  });
 
   useLayoutEffect(() => {
     document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("app-theme", theme);
   }, [theme]);
 
   return [theme, setTheme];
